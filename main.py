25cc146baeff634210920d52a665cf0f31ba4cc2
Torben Funck <mail@tfunck.de>
2021-08-23 12:35:23 +0200

    added counter and output for it

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glob
 import xml.etree.ElementTree as ET
 
 filenames = glob.glob("parseJMeter\\response\\*.soap+xml") #get all files in response directory
+count = 0
 
 for filename in filenames:
     with open(filename, 'r', encoding="utf-8") as content:
@@ -12,3 +13,6 @@ for filename in filenames:
         else: #if array-position 1 in x does not contain "sendMessageResponse" we continue
             with open('parseJMeter\\jmeter_ids.csv', 'a') as f:
                 f.write(x[6].text + '\n') #array-position 6 in x gets written to jmeter_ids.csv
+            count = count + 1
+
+print(f"Es wurden {count} MessageIDs gefunden")
